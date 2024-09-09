58cc5ca57b4c078e3d910cf88c9e99e5430f99e7
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-09-09 22:49:59 +0800

    Create RenderBlocks.jsx

---
+++ payload-nextjs-frontend/src/utils/RenderBlocks.jsx
@@ -0,0 +1,19 @@
+import { blocks } from "@/blocks/blockList";
+import React from "react";
+
+export default function RenderBlocks ({layout}) {
+    <div>
+        {
+            layout.map((block, i) => {
+                // Check the blocks in the blockList
+                const Block = blocks[block.blockType]; // blockType is the slug in the backend
+                if (Block) {
+                    // Render block if mapped slug is found
+                    return <Block key={i}  {...block} />
+                }
+
+                return null;
+            })
+        }
+    </div>
+}
